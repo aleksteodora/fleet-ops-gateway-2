5382d71ba3591421851ceb95bec591215731d224
Teodoraa7 <aleksic.sv7.2023@uns.ac.rs>
2026-09-09 12:47:23 +0200

    feature(domain): add initial REST controller structure

---
+++ fleet-ops-gateway/src/main/java/rs/tiacgroup/fleetopsgateway/searchhistory/controller/SearchHistoryController.java
@@ -0,0 +1,9 @@
+package rs.tiacgroup.fleetopsgateway.searchhistory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/search-history")
+public class SearchHistoryController {
+}
---
+++ fleet-ops-gateway/src/main/java/rs/tiacgroup/fleetopsgateway/vehiclesearch/controller/VehicleSearchController.java
@@ -0,0 +1,9 @@
+package rs.tiacgroup.fleetopsgateway.vehiclesearch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vehicle-search")
+public class VehicleSearchController {
+}
